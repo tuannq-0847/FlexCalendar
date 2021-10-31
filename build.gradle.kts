d3f82e4558d0b6dfae964ef38e0f53b1e6296b77
tuannq-0847 <fckurmother770@gmail.com>
2021-10-31 11:07:59 +0700

    update some lib, fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
+plugins {
+    kotlin("jvm").version("1.4.31")
+}
+
 buildscript {
-    val kotlin_version by extra("1.3.72")
+//    val kotlin_version by extra("1.3.72")
     repositories {
         google()
         jcenter()
 
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("com.android.tools.build:gradle:4.0.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.10.1")
@@ -17,11 +21,5 @@ allprojects {
     repositories {
         google()
         jcenter()
-    }
-}
-
-tasks {
-    val clean by registering(Delete::class) {
-        delete(buildDir)
     }
 }
--- calendar/build.gradle.kts
+++ calendar/build.gradle.kts
@@ -12,12 +12,12 @@ val group = project.property("group") as String
 val version = project.property("version") as String
 
 android {
-    compileSdkVersion(29)
-    buildToolsVersion = "29.0.2"
+    compileSdkVersion(31)
+    buildToolsVersion = "29.0.3"
 
     defaultConfig {
         minSdkVersion(23)
-        targetSdkVersion(29)
+        targetSdkVersion(31)
         versionCode = 1
         versionName = version
 
@@ -33,17 +33,17 @@ android {
 }
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72")
-    testImplementation("junit:junit:4.13")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.core:core-ktx:1.3.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+//    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.32")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    testImplementation("junit:junit:4.13.2")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.core:core-ktx:1.3.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
     //leak canary
-    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
-
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
+    implementation("com.jakewharton.threetenabp:threetenabp:1.2.1")
     implementation("com.auth0.android:jwtdecode:2.0.0")
 }
 
